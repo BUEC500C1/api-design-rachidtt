a03896613917dfd9c403849809a0adff21530e1e
rachidtt <rachidtt@bu.edu>
2020-01-30 20:58:09 -0500

    separated tests

--- tests.py
+++ tests.py
@@ -2,13 +2,15 @@ import pytest
 from airportweather import getWeatherFromCoord
 
 
-def test_getWeatherFromCoord():
+def test_getWeatherFromCoord_lat():
 	with pytest.raises(ValueError):
 		getWeatherFromCoord('100','50')
 
+def test_getWeatherFromCoord_long():
 	with pytest.raises(ValueError):
 		getWeatherFromCoord('10','500')
-
+		
+def test_getWeatherFromCoord_string():
 	assert getWeatherFromCoord('a','z')==False
 
 
